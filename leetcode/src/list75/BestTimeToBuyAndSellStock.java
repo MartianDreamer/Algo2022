476443285d8a77eb68388be23837644b5f3103aa
Sang Nguyen <nguyenxuansang9494@gmail.com>
2022-06-29 18:37:08 +0700

    Best time to buy and sell stock

---
+++ leetcode/src/list75/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,20 @@
+package list75;
+
+public class BestTimeToBuyAndSellStock {
+
+
+    public int maxProfit(int[] prices) {
+        int currentMin = Integer.MAX_VALUE;
+        int maxP = 0;
+        for (int i = 0; i < prices.length; i++) {
+            currentMin = Math.min(currentMin, prices[i]);
+            maxP = Math.max(maxP, prices[i] - currentMin);
+        }
+        return maxP;
+    }
+
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+        System.out.println(bestTimeToBuyAndSellStock.maxProfit(new int[]{23, 27, 28, 8, 15, 22, 2, 5, 7}));
+    }
+}
