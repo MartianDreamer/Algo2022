d9de3ec0efe50ac2d0026ac14ea8da32d25aff7e
Sang Nguyen <nguysa02@risk.regn.net>
2022-06-29 15:18:00 +0700

    Queue Reconstruction By Height with better idea

--- leetcode/src/daily/QueueReconstructionByHeight.java
+++ leetcode/src/daily/QueueReconstructionByHeight.java
@@ -1,5 +1,6 @@
 package daily;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 //Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
@@ -8,25 +9,11 @@ import java.util.Arrays;
 
 public class QueueReconstructionByHeight {
     public int[][] reconstructQueue(int[][] people) {
-        Arrays.sort(people, (a, b) -> a[1] * 1000000 + a[0] - b[1] * 1000000 - b[0]);
-        for (int i = 1; i < people.length; i++) {
-            if (people[i][1] != people[i-1][1]) {
-                int count = 0;
-                for (int j = 0; j < i; j++) {
-                    if (people[j][0] >= people[i][0]) {
-                        count++;
-                    }
-                }
-                int swapi = i;
-                while(count != people[swapi][1]) {
-                    int[] temp = people[swapi-1];
-                    people[swapi-1] = people[swapi];
-                    people[swapi] = temp;
-                    count--;
-                    swapi--;
-                }
-            }
+        Arrays.sort(people, (a, b) -> b[0] * people.length + a[1] - a[0] * people.length - b[1]);
+        ArrayList<int[]> list = new ArrayList<>();
+        for (var per : people) {
+            list.add(per[1], per);
         }
-        return people;
+        return list.toArray(new int[][]{});
     }
 }
