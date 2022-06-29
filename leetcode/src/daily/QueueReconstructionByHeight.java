d90d10dd25ced4e4d7eedf04e2dc48f73987e279
Sang Nguyen <nguysa02@risk.regn.net>
2022-06-29 15:38:10 +0700

    add link to the problem

--- leetcode/src/daily/QueueReconstructionByHeight.java
+++ leetcode/src/daily/QueueReconstructionByHeight.java
@@ -3,9 +3,7 @@ package daily;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-//Input: people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
-//        Output: [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
-// Bad version, although it works...
+//https://leetcode.com/problems/queue-reconstruction-by-height/
 
 public class QueueReconstructionByHeight {
     public int[][] reconstructQueue(int[][] people) {
