d1a4c451bfb8619e8d7d2ab43bb2848231d6f937
Sang Nguyen <nguyenxuansang9494@gmail.com>
2022-06-30 23:13:53 +0700

    ProductOfArrayExceptSelf

---
+++ leetcode/src/list75/ProductOfArrayExceptSelf.java
@@ -0,0 +1,31 @@
+package list75;
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                int numsi = 1;
+                for (int j = 0; j < nums.length; j++) {
+                    if (j == i)
+                        continue;
+                    numsi *= nums[j];
+                    nums[j] = 0;
+                }
+                nums[i] = numsi;
+                return nums;
+            }
+        }
+        int nums0 = 1;
+        int originalNumsi = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums0 *= nums[i];
+        }
+        nums[0] = nums0;
+        for (int i = 1; i < nums.length; i++) {
+            int temp = nums[i];
+            nums[i] = nums[i-1]*originalNumsi/nums[i];
+            originalNumsi = temp;
+        }
+        return nums;
+    }
+}
