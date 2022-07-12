ff858d593a09b7dac8996b6539440d3162731580
Sang Nguyen <nguyenxuansang9494@gmail.com>
2022-07-13 00:07:38 +0700

    Better solution for MaximumProductSubarray

--- leetcode/src/list75/MaximumProductSubarray.java
+++ leetcode/src/list75/MaximumProductSubarray.java
@@ -3,46 +3,25 @@ package list75;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-// Bad solution
+
 public class MaximumProductSubarray {
     public int maxProduct(int[] nums) {
-        List<Integer> l = new ArrayList<>();
-        int start = 0;
+        int max  = Integer.MIN_VALUE;
+        int current = 1;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0 || i == nums.length-1) {
-                l.add(calculate(nums, start, i));
-                start = i + 1;
-            }
+            if (current == 0)
+                current = 1;
+            current *= nums[i];
+            max = Math.max(max, current);
         }
-        return l.stream().max(Comparator.comparingInt(a -> a)).orElse(0);
-    }
-    public int calculate(int[] nums, int start, int end) {
-        int maxNegativeI = start;
-        int minNegativeI = start;
-        for (int i = start + 1; i <= end; i++) {
-            nums[i] *= nums[i-1];
-            if (nums[minNegativeI] > 0 && nums[i] < 0) {
-                maxNegativeI = i;
-                minNegativeI = i;
-            }
+        current = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (current == 0)
+                current = 1;
+            current *= nums[i];
+            max = Math.max(max, current);
         }
-        int defValue = start;
-        for (int i = start; i <= end; i++) {
-            if (nums[i] >= 0) {
-                if (nums[i] > nums[defValue])
-                    defValue = i;
-            } else {
-                if (nums[i] <= nums[minNegativeI]) {
-                    minNegativeI = i;
-                }
-                if (nums[i] > nums[maxNegativeI]) {
-                    maxNegativeI = i;
-                }
-            }
-        }
-        if (minNegativeI == maxNegativeI || nums[minNegativeI] > 0)
-            return nums[defValue];
-        return Math.max(nums[defValue], nums[minNegativeI]/nums[maxNegativeI]);
+        return max;
     }
 
     public static void main(String[] args) {
