d5cafb70cae472707ac72fc19515a333728033b8
Sang Nguyen <nguysa02@risk.regn.net>
2022-07-14 10:23:00 +0700

    Better solution for ThreeSum

--- leetcode/src/list75/array/ThreeSum.java
+++ leetcode/src/list75/array/ThreeSum.java
@@ -3,34 +3,37 @@ package list75.array;
 import java.util.*;
 import java.util.stream.Collectors;
 
-//Bad solution
 
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> rs = new ArrayList<>();
-        Set<Integer> passedI = new HashSet<>();
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (passedI.contains(nums[i]))
+        Arrays.sort(nums);
+        List<List<Integer>> rs = new LinkedList<>();
+        int start = 0;
+        int end = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            start = i + 1;
+            end = nums.length - 1;
+            if (i != 0 && nums[i] == nums[i - 1])
                 continue;
-            Map<Integer, Integer> met = new HashMap<>();
-            for (int j = i + 1; j < nums.length; j++) {
-                if (passedI.contains(nums[j]))
-                    continue;
-                if (met.getOrDefault(-nums[i] - nums[j], 0) == 1) {
-                    rs.add(List.of(nums[i], -nums[i]-nums[j], nums[j]));
-                    met.put(-nums[i] - nums[j], 0);
-                    met.putIfAbsent(nums[j], 0);
-                } else {
-                    met.putIfAbsent(nums[j], 1);
-                }
+            while (start < end) {
+                if (nums[start] + nums[end] + nums[i] == 0) {
+                    rs.add(List.of(nums[i], nums[start], nums[end]));
+                    while (start < end && nums[start + 1] == nums[start])
+                        start++;
+                    while (start < end && nums[end - 1] == nums[end])
+                        end--;
+                    start++;
+                } else if (nums[start] + nums[end] + nums[i] < 0) {
+                    start++;
+                } else
+                    end--;
             }
-            passedI.add(nums[i]);
         }
         return rs;
     }
 
     public static void main(String[] args) {
         ThreeSum threeSum = new ThreeSum();
-        System.out.println(threeSum.threeSum(new int[]{-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0}));
+        System.out.println(threeSum.threeSum(new int[]{-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0}));
     }
 }
