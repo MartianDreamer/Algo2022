c6d589833b9baf4b2ab696cf24c8a241ddffb8b3
Sang Nguyen <nguyenxuansang9494@gmail.com>
2022-07-18 21:16:44 +0700

    SumOfTwoIntegers

--- leetcode/src/list75/binary/SumOfTwoIntegers.java
+++ leetcode/src/list75/binary/SumOfTwoIntegers.java
@@ -2,16 +2,14 @@ package list75.binary;
 
 public class SumOfTwoIntegers {
     public int getSum(int a, int b) {
-        if (a == 0)
-            return b;
-        if (b == 0)
-            return a;
-        int rem = (a&b) << 1;
-        return getSum(a^b,rem);
+        int rem = (a & b) << 1;
+        if (rem == 0)
+            return a ^ b;
+        return getSum(a ^ b, rem);
     }
 
     public static void main(String[] args) {
         SumOfTwoIntegers sumOfTwoIntegers = new SumOfTwoIntegers();
-        System.out.println(sumOfTwoIntegers.getSum(17,-12));
+        System.out.println(sumOfTwoIntegers.getSum(17, -12));
     }
 }
