6df5b770cf1d7d8896cb0c281550bb62168e28f6
MarsLover <notreal@mail.com>
2023-02-05 16:28:42 +0700

    LongestIncreasingSubsequence

---
+++ leetcode/src/list75/dynamicprogramming/LongestIncreasingSubsequence.java
@@ -0,0 +1,19 @@
+package list75.dynamicprogramming;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+    // naive implementation
+    public int lengthOfLIS(int[] nums) {
+        int[] counts = new int[nums.length];
+        Arrays.fill(counts, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && counts[j] + 1 > counts[i]) {
+                    counts[i] = counts[j] + 1;
+                }
+            }
+        }
+        return Arrays.stream(counts).max().orElse(1);
+    }
+}
